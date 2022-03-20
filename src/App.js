af8d29a447bff61575cb73bebd8dcdae81266bc7
Monica Delgado <mnca.delgado@gmail.com>
2022-03-19 18:00:11 -1100

    added jsx to the App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import * as React from 'react';
 
+function getTittle (title) {
+  return title;
+}
+
+const location = 'is here';
+
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {getTittle('React')} {location}</h1>
+      <h2>{welcome.greeting} {welcome.title}</h2>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
     </div>
     );   
 }
