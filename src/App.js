c4094afd1837e6279a9365b45bef8deca87d2bbd
Monica Delgado <mnca.delgado@gmail.com>
2022-04-18 23:16:45 -1100

    handled props with object destructuring and lifted the state up to the App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+
+
 const App = () => {
   const stories = [
     {
@@ -18,61 +20,83 @@ const App = () => {
       objectID: 1,
     },
   ];
+  //moved (lifted) the state up to the closest common parent component (App), and then
+  //passed down the state that comes from the input field as a prop (with the handlerSeach function)
+  const [searchTerm, setSearchTerm] = React.useState('');
 
+//Function that passes down the state to our Search component 
   const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
     console.log(event.target.value);
   };
+
+  //This variable filters our list (array of objects) when the user changes the state by typing 
+  //information on the input field 
+  const searchedStories = stories.filter((story) =>
+  story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-
-      <Search onSearch={handleSearch} />
+      {/*Passed the searchTerm as a prop in order to show the current state in 
+      the Search component*/}
+      <Search onSearch={handleSearch} searchTerm={searchTerm}/>
 
       <hr />
+
       
-      <List list={stories} />
+      <List list={searchedStories} />
 
     </div>
   );
 ;}
 
-const Search = (props) => {
-  const [searchTerm, setSearchTerm] = React.useState('');
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
 
-    props.onSearch(event)
+//Used object destructuring with the props from the App component as parameters
+const Search = ({searchTerm, onSearch}) => {
+  
+  const handleChange = (event) => {
+    onSearch(event)
   };
 
   return (
     <div>
     <label htmlFor="search">Search: </label>
-    <input id="search" type="text" onChange={handleChange}/>
+    <input 
+    id="search" 
+    type="text"
+    //this attribute lets you controlled the state of your component  
+    value={searchTerm}
+    onChange={handleChange}/>
 
     <p>
-      Searching for <strong>{searchTerm}</strong>
+      Searching for: <strong>{searchTerm}</strong>
     </p>
     </div>
   );
 };
 
-const List = (props) => (
+
+//object destructuring with the props from the app component 
+const List = ({list}) => (
      <ul>
-        {props.list.map((item) => (
+        {list.map((item) => (
           <Item key={item.objectID} item={item}/>
       ))}
     </ul>
 );
 
-const Item = (props) => (
+
+//object destructuring...
+const Item = ({item}) => (
   <li>
     <span>
-      <a href={props.item.url}>{props.item.title}</a>
+      <a href={item.url}>{item.title}</a>
    </span>
-   <span>{props.item.author}</span>
-   <span>{props.item.num_comments}</span>
-   <span>{props.item.points}</span>
+   <span>{item.author}</span>
+   <span>{item.num_comments}</span>
+   <span>{item.points}</span>
   </li>
 );
 
