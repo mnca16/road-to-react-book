790db968a7586c3790a17943c44f264dfc6f9d9e
Monica Delgado <mnca.delgado@gmail.com>
2022-06-01 10:28:26 -1100

    manage the submith button with a new useState

--- src/App.js
+++ src/App.js
@@ -60,6 +60,8 @@ const App = () => {
   //in sync with the browser's local storage.
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
 
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
+
   //UseReducer hook, which manage multiple states
   const [stories, dispatchStories] = React.useReducer(storiesReducer, {
     data: [],
@@ -72,7 +74,7 @@ const App = () => {
 
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    fetch(`${API_ENDPOINT}${searchTerm}`)
+    fetch(url)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -81,7 +83,7 @@ const App = () => {
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
-  }, [searchTerm]);
+  }, [url]);
 
   React.useEffect(() => {
     handleFetchStories();
@@ -95,8 +97,12 @@ const App = () => {
   };
 
   //Function that passes down the state to our Search component
-  const handleSearch = (event) => {
+  const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
   };
 
   return (
@@ -109,7 +115,7 @@ const App = () => {
         //label="Search"
         value={searchTerm}
         isFocused
-        onInputChange={handleSearch}
+        onInputChange={handleSearchInput}
       >
         Search:
       </InputWithLabel>
