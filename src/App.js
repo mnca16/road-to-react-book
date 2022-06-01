a762b20a25327ad3928a3ed5b07dbe82ef707fbd
Monica Delgado <mnca.delgado@gmail.com>
2022-06-01 11:57:54 -1100

    added the SearchForm component and I'm finnaly update with the material!!

--- src/App.js
+++ src/App.js
@@ -102,28 +102,20 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+    event.preventDeafault();
   };
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      {/*Passed the searchTerm as a prop in order to show the current state in 
-      the Search component*/}
-      <InputWithLabel
-        id="search"
-        //label="Search"
-        value={searchTerm}
-        isFocused
-        onInputChange={handleSearchInput}
-      >
-        Search:
-      </InputWithLabel>
 
-      <button type="button" disabled={!searchTerm} onClick={handleSearchSubmit}>
-        Submit
-      </button>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       <hr />
 
@@ -137,6 +129,23 @@ const App = () => {
     </div>
   );
 };
+
+const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+);
 
 const InputWithLabel = ({
   id,
@@ -183,11 +192,6 @@ const List = ({ list, onRemoveItem }) => (
 );
 
 const Item = ({ item, onRemoveItem }) => (
-  //This is another way to remove an item without the inline handler
-  //This is a normal handlers
-  // const handleRemoveItem = () => {
-  //   onRemoveItem(item);
-  // };
   <li>
     <span>
       <a href={item.url}>{item.title}</a>
