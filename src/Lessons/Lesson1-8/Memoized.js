42957f94695cc772f4fe5472876539246771c629
Monica Delgado <mnca.delgado@gmail.com>
2022-06-01 09:56:38 -1100

    added memoized practice (closures) in plain js to the folder lesson 1.8

--- src/Lessons/Lesson1-8/Memoized.js
+++ src/Lessons/Lesson1-8/Memoized.js
@@ -1,4 +1,5 @@
 //This is going to be clousers in plain JavaScript
+
 //Factory Functions
 
 function createTaxCalculator(tax) {
@@ -14,3 +15,8 @@ const calculateIncomeTaxAmount = createTaxCalculator(0.25);
 
 console.log(calculateVaAmount(100));
 console.log(calculateVaAmount(200));
+
+//Practice make your on example of this, if we call calculateTaximcome
+//do they overwrite the previous argument? 100 or 200
+
+//Closures
