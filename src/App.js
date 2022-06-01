b030e835a5b5bc28ae3ac78890e64e3ba2bd47d2
Monica Delgado <mnca.delgado@gmail.com>
2022-05-31 16:38:21 -1100

    fetched the data with the built in fetch API

--- src/App.js
+++ src/App.js
@@ -112,15 +112,26 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
   }, []);
+
+  //   getAsyncStories()
+  //     .then((result) => {
+  //       dispatchStories({
+  //         type: "STORIES_FETCH_SUCCESS",
+  //         payload: result.data.stories,
+  //       });
+  //     })
+  //     .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
+  // }, []);
 
   //
   const handleRemoveStory = (item) => {
