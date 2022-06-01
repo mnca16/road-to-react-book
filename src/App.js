18a93e5e5ca4b3a01d3356fb176393f2a891ef5a
Monica Delgado <mnca.delgado@gmail.com>
2022-06-01 11:28:35 -1100

    replaced fetch API with axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import axios from "axios";
 
 //Custom hook used in the app component
 const useSemiPersistentState = (key, initialState) => {
@@ -74,12 +75,12 @@ const App = () => {
 
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    fetch(url)
-      .then((response) => response.json())
+    axios
+      .get(url)
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
